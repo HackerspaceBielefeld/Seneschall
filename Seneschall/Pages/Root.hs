8154acec527999f7b862e47aafa2dbb58608cf70
Jürgen Peters <taulmarill@xgn.de>
2014-08-08 17:52:12 +0200

    fixing Pages.Root

--- Seneschall/Pages/Root.hs
+++ Seneschall/Pages/Root.hs
@@ -13,9 +13,9 @@ import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5.Attributes as A
 
 root :: ReqM ()
-root = do 
+root = do
     logged <- elem User <$> roles <$> lift get
-    template $ H.div ! A.class_ "centerBox" $ if logged then loginForm else ""
+    template $ H.div ! A.class_ "centerBox" $ if logged then "" else loginForm
 
 loginForm :: H.Html
 loginForm = H.form ! A.action "/login" ! A.method "POST" $ do
